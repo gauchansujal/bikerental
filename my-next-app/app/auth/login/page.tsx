0e1b65fb97110ae4720d70748a09c82cd8a53b6b
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2025-12-27 22:02:32 +0545

    logints addede

---
+++ my-next-app/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from '../components/loginForm'
+
+export default function LoginPage() {
+  return (
+    <div className="max-w-md mx-auto mt-20">
+      <h1 className="text-2xl font-bold mb-6 text-center">Login</h1>
+      <LoginForm />
+    </div>
+  )
+}
