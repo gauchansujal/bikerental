ffa1738da4e198eccc9887d1071d4a92640882df
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2026-02-09 04:32:13 +0545

    added

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,10 +1,20 @@
 import { Router } from "express";
 import { AuthController } from "../controllers/auth.controller";
-
+import { authorizationMiddleware } from "../middlewares/authorized.middleware";
+import { uploads } from "../middlewares/uplode.middlewares";
 const router = Router();
 const controller = new AuthController();
-
+let authController = new AuthController();
 router.post("/register", controller.register);
 router.post("/login", controller.login);
+router.get("/whoami", authorizationMiddleware, authController.getProfile);
+
+router.put(
+    "/update-profile",
+    authorizationMiddleware,
+    uploads.single("image"), // "image" - field name from frontend/client
+    authController.updateProfile
+)
+
 
 export default router;
