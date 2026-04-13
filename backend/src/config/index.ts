d16f5cf10cd13f15ad6a36d76958dfe4b6e8de3d
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2026-04-13 16:47:50 +0545

    addedE

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv';
-dotenv.config();
-
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config();
+}
 export const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 5050;
-export const MONGODB_URI: string = process.env.MONGODB_URI!;
+export const MONGODB_URL: string = process.env.MONGODB_URL!;
 export const CLIENT_URL: string = process.env.CLIENT_URL || 'http://localhost:3000';
 export const EMAIL_USER: string = process.env.EMAIL_USER!;
 export const EMAIL_PASS: string = process.env.EMAIL_PASS!;
