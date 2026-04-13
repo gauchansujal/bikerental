fb25c7bd56da9450e4e30caa848971f09be35226
gauchansujal <151667820+gauchansujal@users.noreply.github.com>
2026-04-13 15:32:58 +0545

    added

--- backend/src/config/index.ts
+++ backend/src/config/index.ts
@@ -1,20 +1,9 @@
-
-
 import dotenv from 'dotenv';
-dotenv.config(); 
+dotenv.config();
 
-//Application level constant and config
 export const PORT: number = process.env.PORT ? parseInt(process.env.PORT) : 5050;
-// IF POrt is not defined in .env use 5050 as deafulat
-  export const MONGODB_URI: string = process.env.MONGODB_URI || 'mongodb+srv://gauchansujal0_db_user:test123@cluster0.qsdgkxi.mongodb.net/web_api';
-  
-  export const CLIENT_URL: string =
-process.env.CLIENT_URL || 'http://localhost:3000';
-   
-  export const EMAIL_USER: string =
-    process.env.EMAIL_USER || 'gauchansujal0@gmail.com'
-  export const EMAIL_PASS:string = process.env.EMAIL_PASS || 'ratw gasn nxla xmti';
-
-    export const JWT_SECRET: string =  process.env.JWT_SECRET|| '97684';
-
-    //if MONGODB-UTI is not defined in .env, use local.backup mongodb as default
+export const MONGODB_URI: string = process.env.MONGODB_URI!;
+export const CLIENT_URL: string = process.env.CLIENT_URL || 'http://localhost:3000';
+export const EMAIL_USER: string = process.env.EMAIL_USER!;
+export const EMAIL_PASS: string = process.env.EMAIL_PASS!;
+export const JWT_SECRET: string = process.env.JWT_SECRET!;
